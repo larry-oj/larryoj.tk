779c8ee1db25c098638bfbf1b1d97de9b86e354c
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-05 13:30:32 +0300

    add resume, fix phone

--- js/sctript.js
+++ js/sctript.js
@@ -1,6 +1,6 @@
 const phone = document.getElementById('phone');
 phone.getElementsByTagName('a')[0].onclick = function () {
-    phone.innerHTML = '<a href="tel:+380500000000">+380500000000</a>';
+    phone.innerHTML = '<a href="tel:+380507706434">+380 (50) 770-64-34</a>';
 }
 
 const email = document.getElementById('email');
